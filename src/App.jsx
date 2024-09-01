41f850102bf4aa2df1afbb26882f6b684788b05d
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-09-01 19:06:17 +0300

    edited socket io url again

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const initialGameBoard = [
   [null, null, null]
 ];
 
-const socket = io('https://tic-tac-toe-eight-phi-17.vercel.app/'); // Connect to the server
+const socket = io('https://tic-tac-toe-eight-phi-17.vercel.app/api/server'); // Connect to the server
 
 // Determine the active player
 function deriveActivePlayer(gameTurns) {
